f34e474328f6779f88785c2d3d5cea5e1872fa21
Vishwa Ram <iamvishwaramj@gmail.com>
2025-11-26 13:09:12 +0530

    home

--- Shoe-Ecom/src/App.jsx
+++ Shoe-Ecom/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import Nav from "./component/Nav";
+import Home from "./component/Home"
 
 // import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <Nav />
+      <Home/>
     </>
   );
 }
---
+++ Shoe-Ecom/src/component/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="f-row">
+        <div className="card-image1">
+          <img
+            src="https://themewagon.github.io/stylish/images/card-image1.jpg"
+            alt=""
+          />
+        </div>
+        <div className="card-image2">
+          <img
+            src="https://themewagon.github.io/stylish/images/card-image2.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
